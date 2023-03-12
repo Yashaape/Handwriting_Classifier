fb8d34218d68f9b9dd80ba0b5b6a6bb58e873825
brandon <brasgaitis97@gmail.com>
2023-03-11 19:32:35 -0800

    beginning of node class

--- ANN.py
+++ ANN.py
@@ -2,17 +2,47 @@ import numpy as np
 
 
 class node:
-    def __int__(self, dataval=None):
-        self.dataval = dataval
-        self.nextval = None
+    def __int__(self):
+        self.collector = 0.0
+        self.connections = []
 
+    def __str__(self):
+        str_out = self.collector
 
 net = []
-
+network = "4,2,1"
 output_layer = None
-collector = 0.0
 
-with open('input.txt') as num:
-    network = num.read().splitlines()
+network = network.split(',')
+network_struct = np.array([network]).T
+print(network_struct)
 
-print(network)
+
+
+N_struct = []
+with open('network.txt', 'r') as Net:
+    #print(Net.read())
+
+
+    # N_struct = Net.read().split(',')
+    # print(N_struct)
+
+    tmp = Net.read().split(',')
+    for t in tmp:
+        N_struct.append(int(t.strip()))
+
+    Network = []
+    for i in N_struct:
+        for j in range(i):
+            tmp.append([])
+    print(tmp)
+
+# with open('input.txt') as num:
+#     Inputs = np.array(num.read().splitlines())
+#     print(Inputs)
+input_layer = []
+with open('input.txt', 'r') as num:
+    first_layer = num.read().split(',')
+    print(first_layer)
+    for i in range(len(first_layer)):
+        N_struct[0][i]
