54afa33d8a250280bd5a017faec06b842dac82e6
brandon <brasgaitis97@gmail.com>
2023-03-11 18:01:39 -0800

    beginning of node class

--- ANN.py
+++ ANN.py
@@ -5,3 +5,14 @@ class node:
     def __int__(self, dataval=None):
         self.dataval = dataval
         self.nextval = None
+
+
+net = []
+
+output_layer = None
+collector = 0.0
+
+with open('input.txt') as num:
+    network = num.read().splitlines()
+
+print(network)
