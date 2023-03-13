60ac5deae3a4352bbf741f79492f8e58e4aa9eee
brandon <brasgaitis97@gmail.com>
2023-03-13 14:24:44 -0700

    network structure created

--- ANN.py
+++ ANN.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class node:
     def __int__(self):
         self.collector = 0.0
@@ -17,32 +16,15 @@ network = network.split(',')
 network_struct = np.array([network]).T
 print(network_struct)
 
-
-
-N_struct = []
-with open('network.txt', 'r') as Net:
-    #print(Net.read())
-
-
-    # N_struct = Net.read().split(',')
-    # print(N_struct)
-
-    tmp = Net.read().split(',')
-    for t in tmp:
-        N_struct.append(int(t.strip()))
-
-    Network = []
-    for i in N_struct:
-        for j in range(i):
-            tmp.append([])
-    print(tmp)
-
-# with open('input.txt') as num:
-#     Inputs = np.array(num.read().splitlines())
-#     print(Inputs)
-input_layer = []
 with open('input.txt', 'r') as num:
-    first_layer = num.read().split(',')
+    first_layer = np.array([num.read().split(',')]).astype(float)
     print(first_layer)
-    for i in range(len(first_layer)):
-        N_struct[0][i]
+
+for i in network_struct:
+    net.append([int(i)])
+    print(net)
+
+for i in range(int(first_layer)):
+    net.append(network_struct[0][i])
+    print(net)
+
