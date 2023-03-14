518b14e423eaa45a8eaef038cdd0dfe43be74005
brandon <brasgaitis97@gmail.com>
2023-03-14 16:51:15 -0700

    input list and network lists created

--- ANN.py
+++ ANN.py
@@ -5,26 +5,42 @@ class node:
         self.collector = 0.0
         self.connections = []
 
-    def __str__(self):
-        str_out = self.collector
-
 net = []
 network = "4,2,1"
 output_layer = None
-
 network = network.split(',')
 network_struct = np.array([network]).T
-print(network_struct)
-
-with open('input.txt', 'r') as num:
-    first_layer = np.array([num.read().split(',')]).astype(float)
-    print(first_layer)
-
+#print(network_struct)
+print("Network Structure:")
 for i in network_struct:
-    net.append([int(i)])
+    net.append(int(i))
     print(net)
+print()
 
-for i in range(first_layer.shape[0]):
-    net.append(network_struct[0][i])
-    print(net)
+input= []
+print("Inputs:")
+with open('input.txt', 'r') as num:
+    first_layer = np.array(num.read().split(',')).T
+    #print(first_layer, '\n')
+    for i in first_layer:
+        input.append(float(i))
+        print(input)
 
+
+
+# inputs = []
+# with open('input.txt', 'r') as inp:
+#     tmp = inp.read().split(',')
+#     for t in tmp:
+#         inputs.append(float(t.strip()))
+#         print(inputs)
+# for i in range(first_layer.shape[0]):
+#     net.append(network_struct[0][i])
+#     print(net)
+
+# network_struct = []
+# with open('network.txt', 'r') as net:
+#     tmp = net.read().split(',')
+#     for t in tmp:
+#         network_struct.append(int(t.strip()))
+#         print(network_struct)
