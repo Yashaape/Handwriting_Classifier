d74de9c049134b8a5dd6f71ff5cc4f484ccfdab1
brandon <brasgaitis97@gmail.com>
2023-03-14 16:53:46 -0700

    input list and network lists created

--- ANN.py
+++ ANN.py
@@ -27,20 +27,3 @@ with open('input.txt', 'r') as num:
         print(input)
 
 
-
-# inputs = []
-# with open('input.txt', 'r') as inp:
-#     tmp = inp.read().split(',')
-#     for t in tmp:
-#         inputs.append(float(t.strip()))
-#         print(inputs)
-# for i in range(first_layer.shape[0]):
-#     net.append(network_struct[0][i])
-#     print(net)
-
-# network_struct = []
-# with open('network.txt', 'r') as net:
-#     tmp = net.read().split(',')
-#     for t in tmp:
-#         network_struct.append(int(t.strip()))
-#         print(network_struct)
