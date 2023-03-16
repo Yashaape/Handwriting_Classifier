f85e5ed68ac9449bbdbae01a8a97eaa764f0a3e0
brandon <brasgaitis97@gmail.com>
2023-03-16 13:44:25 -0700

    converted to numpy to clean up code

--- ANN.py
+++ ANN.py
@@ -1,44 +1,52 @@
 import numpy as np
 
 class node:
-    def __init__(self):
+    def __init__(self, lastlayer = None):
+        self.lastlayer = lastlayer
         self.collector = 0.0
         self.connections = []
 
 def main():
-    net_structure = []
-    network = "4,2,1"
-    output_layer = None
-    network = network.split(',')
-    network = np.array([network]).T
-    #print(network)
     print("Network Structure:")
-    for i in network:
-        net_structure.append(int(i))
-        print(net_structure)
-
-
+    net_structure = np.array([4,2,1], dtype=np.int32)
+    print(net_structure)
+    output_layer = None
     net = []
-    nd = node()
+    #nd = node()  This was the cause of my sum being incorrect creating the node outside the loop causes sum errors
     for i in net_structure:
-        tmp = []
+        first_layer = []
         for j in range(i):
-            tmp.append(nd)
-        net.append(tmp)
-        output_layer = tmp
-    print(net)
-    print()
-    print(output_layer)
+            first_layer.append(node(output_layer))
+        net.append(first_layer)
+        output_layer = first_layer
+    print("network: ", net)
+    print("output layer: ",output_layer) #Output layer aka last layer
     print()
 
-    input_data = []
+
+    # net = []
+    # nd = node()
+    # for i in net_structure:
+    #     tmp = []
+    #     for j in range(i):
+    #         tmp.append(nd)
+    #     net.append(tmp)
+    #     output_layer = tmp
+    # print(net)
+    # print()
+    # print(output_layer)
+    # print()
     print("Inputs:")
-    with open('input.txt', 'r') as num:
-        first_layer = np.array(num.read().split(','))
-        #print(first_layer, '\n')
-        for i in first_layer:
-            input_data.append(float(i))
-            print(input_data)
+    input_data = np.loadtxt('input.txt', delimiter=',', dtype=np.float64)
+    print(input_data)
+
+
+    # with open('input.txt', 'r') as num:
+    #     first_layer = np.array(num.read().split(','))
+    #     #print(first_layer, '\n')
+    #     for i in first_layer:
+    #         input_data.append(float(i))
+    #         print(input_data)
     #print(input_data)
     #print(type(input))
     #print(len(input))
@@ -46,19 +54,19 @@ def main():
 
     for i in range(len(input_data)):
         net[0][i].collector = input_data[i]
-        #print(net[0][i].collector)
+        print(net[0][i].collector)
 
     print()
     print("Sum: ") #doesn't actually provide the correct sum correct sum should be: 46
 
-    print(len(input_data))
-    print(sum(input_data))
+    #print(len(input_data))
+    #print(sum(input_data))
     for i in range(1,len(input_data) - 1, 1):
         #print(i)
         for n in net[i]:
             #print(n)
             #print(net[i-1])
-            for c in net[i-1]:
+            for c in net[i-1]: # replace n.connetions
                 #print(c)
                 n.collector = n.collector + c.collector
 
