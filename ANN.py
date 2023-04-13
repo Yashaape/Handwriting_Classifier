fbe7767e97c8f8c7de71565559f301e0403f78f2
brandon <brasgaitis97@gmail.com>
2023-04-13 09:57:12 -0700

    Created feedforward, and activation functions. Now outputs 0.5 in the output layer

--- ANN.py
+++ ANN.py
@@ -16,24 +16,57 @@ class node:
             self.weights.append(random.random())
         #print(random.random())
 
+net_structure = np.array([4, 2, 1])
+output_layer = None
+net = []
 def sigmoidal(activation):
     return 1.0 / (1.0 + math.exp(-activation))
 def sigmoidal_deriv(output):
     return output * (1.0 - output)
-def main():
-    print("Network Structure:")
-    net_structure = np.array([4,2,1])
-    print(net_structure)
-    output_layer = None
-    net = []
-    #nd = node()  This was the cause of my sum being incorrect creating the node outside the loop causes sum errors
+
+def feed_forward(input_data):
+    for i in range(len(input_data)):
+        if i < len(net[0]):
+            net[0][i].collector = input_data[i]
+            print(net[0][i].collector)
+
+    for layer in net:
+        for n in layer:
+            if n.lastlayer is not None:
+                n.collector = 0.0
+                for index in range(len(n.connections)):
+                    c = n.connections[index]
+                    w = n.weights[index]
+                    n.collector += (c.collector * w)
+                n.collector = sigmoidal(n.collector)
+def activation(network):
+    global output_layer
     for i in net_structure:
         first_layer = []
         for j in range(i):
             first_layer.append(node(lastlayer=output_layer))
+            if output_layer is not None:
+                first_layer[j].weights = [random.random() for i in range(len(output_layer))]
             #print("first layer: ", first_layer)
         net.append(first_layer)
         output_layer = first_layer
+
+
+def main():
+    global output_layer
+    print("Network Structure: ", net_structure)
+    #net_structure = np.array([4,2,1])
+    #output_layer = None
+    #net = []
+    #nd = node()  This was the cause of my sum being incorrect creating the node outside the loop causes sum errors
+    # for i in net_structure:
+    #     first_layer = []
+    #     for j in range(i):
+    #         first_layer.append(node(lastlayer=output_layer))
+    #         #print("first layer: ", first_layer)
+    #     net.append(first_layer)
+    #     output_layer = first_layer
+    activation(net)
     print("network: ", net)
     print("output layer: ",output_layer) #Output layer aka last layer
     print()
@@ -42,10 +75,14 @@ def main():
     input_data = np.loadtxt('input.csv', delimiter=',', dtype=int)
     print(input_data)
 
-    for i in range(len(input_data)):
-        if i < len(net[0]):
-            net[0][i].collector = input_data[i]
-            print(net[0][i].collector)
+    feed_forward(input_data)
+
+    # for i in range(len(input_data)):
+    #     if i < len(net[0]):
+    #         net[0][i].collector = input_data[i]
+    #         print(net[0][i].collector)
+
+
 
     print()
     print("Output Layer: ")
@@ -54,21 +91,21 @@ def main():
     #print(sum(input_data))
 
 
-    for i in np.nditer(input_data):
-        #print(i)
-        for n in net[i]:
-            #print(n)
-            #print(net[i-1])
-            n.connections = net[i-1] # assign n.connections to net[i-1] to avoid summnation errors
-            #print(n.connections)
-
-            if n.connections:
-                n.weights = [random.random()] * len(n.connections)
-            for index in range(len(n.connections)):
-                #print(c)
-                c = n.connections[index]
-                w = n.weights[index]
-                n.collector = n.collector + (c.collector * w)
+    # for i in np.nditer(input_data):
+    #     #print(i)
+    #     for n in net[i]:
+    #         #print(n)
+    #         #print(net[i-1])
+    #         n.connections = net[i-1] # assign n.connections to net[i-1] to avoid summnation errors
+    #         #print(n.connections)
+    #
+    #         if n.connections:
+    #             n.weights = [random.random()] * len(n.connections)
+    #         for index in range(len(n.connections)):
+    #             #print(c)
+    #             c = n.connections[index]
+    #             w = n.weights[index]
+    #             n.collector = n.collector + (c.collector * w)
 
     print(net[2][0].collector)
     with open('doneANN.pickle', 'wb') as handle:
