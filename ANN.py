82e768dbdb6cc07757f26482d21d0877df1a8277
brandon <brasgaitis97@gmail.com>
2023-04-14 12:50:57 -0700

    created initialize_net function and provided test case created new activation function created new forward_propagation function Prior code did not work, forward_prop should be fixed now

--- ANN.py
+++ ANN.py
@@ -2,117 +2,125 @@ import numpy as np
 import random
 import pickle
 import math
-
-class node:
+class Node:
 
     def __init__(self, lastlayer = None):
         self.lastlayer = lastlayer
         self.collector = 0.0
         self.connections = []
-        #self.weights = [0] * len(self.connections)
-        #print(self.weights)
-        if self.connections != None:
-            self.weights = [random.random() for i in range(len(self.connections))]
+        self.weights = []
 
-        # for i in range(len(self.connections)):
-        #     self.weights.append(random.random())
-        # #print(random.random())
-
-net_structure = np.array([4, 2, 1], dtype=np.int64)
+net_structure = np.array([4,2,1])
 output_layer = None
 net = []
-l_rate = 0.1
+
 def sigmoidal(activation):
     return 1.0 / (1.0 + math.exp(-activation))
 
-def sigmoidal_deriv(output): #Part of Back Propagation algorithm
+def sigmoidal_deriv(output):
     return output * (1.0 - output)
 
-def feed_forward(input_data):
-    #print("Input Layer:")
-    for i in range(len(input_data)):
-        if i < len(net[0]):
-            net[0][i].collector = input_data[i]
-            #print(net[0][i].collector)
-
-    for layer in net:
-        for n in layer:
-            if n.lastlayer is not None:
-                n.collector = 0.0
-                for index in range(len(n.connections)):
-                    c = n.connections[index]
-                    w = n.weights[index]
-                    n.collector += (c.collector * w)
-                n.collector = sigmoidal(n.collector)
-
-def compute_error(input_data):
-    expected = input_data[-1]
-    output = [n.collector for n in net[-1]]
-    error = (expected - output[0]) ** 2
-    return error
-
-def update_weights(n, c, error_gradient, lr):
-    index = n.connections.index(c)
-    n.weights[index] += c.collector * error_gradient * lr
-    c.collector = n.weights[index] * error_gradient * sigmoidal(n.collector)
-
-def back_propagation(input_data):
+def initialize_network(network_structure):
     global output_layer
-    #compute_error(input_data)
-    for layer in reversed(net):
-        for n in layer:
-            if n.lastlayer is not None:
-                error_gradient = sigmoidal_deriv(n.collector) * compute_error(input_data)
-                for index in range(len(n.connections)):
-                    c = n.connections[index]
-                    c.error = n.weights[index] * error_gradient  # Update error of connected nodes
-                    update_weights(n, c, error_gradient, lr=l_rate)
-def activation(network):
-    global output_layer
-    for i in net_structure:
-        first_layer = []
+    for i in network_structure:
+        layer = []
         for j in range(i):
-            first_layer.append(node(lastlayer=output_layer))
+            layer.append(Node(lastlayer=output_layer))
             if output_layer is not None:
-                first_layer[j].weights = [random.random() for i in range(len(output_layer))]
-            #print("first layer: ", first_layer)
-        net.append(first_layer)
-        output_layer = first_layer
+                layer[-1].connections = output_layer
+                layer[-1].weights = [{'weights': random.random()} for i in range(len(output_layer))]
+        if output_layer is not None:
+            print(layer[-1].weights)
+            output_weights = [{'output layer weights': random.random()} for i in range(network_structure[2])]
+            print("Output Layer Weights: ", output_weights)
+            output_layer = output_weights
+        net.append(layer)
+        output_layer = layer
+    return net
 
-def train(input_data, lr, epoch, target_error):
-    for e in range(epoch + 1):
-        total_error = 0.0
-        for index in input_data:
-            feed_forward(index[:-1])
-            error = compute_error(index)
-            total_error += error ** 2
-            back_propagation(index)
-        total_error /= len(input_data)
-        print('>epoch=%d, lrate=%.3f, error=%.3f' % (e, lr, total_error))
-        if(total_error <= target_error):
-            print("Target Error reached error=%.3f" % total_error)
-            break
+#Used to visualize adding in weights for each connection in each layer
+# print("Weights and network Initialization:")
+# random.seed(1)
+# net = initialize_network(net_structure)
+# for layer in net:
+#     for node in layer:
+#         if hasattr(node, 'weights') and node.weights:
+#             print(node.weights)
+# print()
 
+def activation(neuron):
+    sum_of_weights = 0.0
+    for index in range(len(neuron.connections)):
+        con = neuron.connections[index]
+        weight = neuron.weights[index]['weights'] # access value from dictionary
+        sum_of_weights += con.collector * weight
+    return sigmoidal(sum_of_weights)
 
-def main():
-    global output_layer
-    print("Network Structure: ", net_structure)
-    activation(net)
-    print("network: ", net)
-    print("output layer: ",output_layer) #Output layer aka last layer
-    print()
-    print("Inputs: ")
-    input_data = np.loadtxt('input.csv', delimiter=',', dtype=int)
-    #print(len(input_data[0])-1)
-    print(input_data, "\n")
+def forward_propagation(network):
+    for layer in network:
+        for node in layer:
+            node.collector = activation(node)
+    return network[-1][0].collector
 
-    print("Train:")
-    train(input_data, lr=l_rate, epoch=10, target_error=0.05)
+#Test case for forward propagation:
+input_values = np.array([0.5982372, 0.000348347, 0.223456, 0.938743784])
+net = initialize_network(net_structure)
 
-    print("\nOutput Layer: ")
+# Assign input values to the collector of input layer nodes
+for i in range(len(input_values)):
+    net[0][i].collector = input_values[i]
 
-    print(net[-1][0].collector)
-    with open('doneANN.pickle', 'wb') as handle:
-        pickle.dump(net, handle)
+output = forward_propagation(net)
+print("Input Values: ", input_values)
+print("Output: ", output)
 
-main()
+# def main():
+#     global output_layer
+#     print("Network Structure:")
+#     print(net_structure)
+#
+#     net = initialize_network(net_structure)
+#     #nd = node()  This was the cause of my sum being incorrect creating the node outside the loop causes sum errors
+#
+#     print("network: ", net)
+#     print("output layer: ",output_layer) #Output layer aka last layer
+#     print()
+#
+#     print("Inputs:")
+#     input_data = np.loadtxt('input.csv', delimiter=',', dtype=int)
+#     print(input_data)
+#
+#     for i in range(len(input_data)):
+#         if i < len(net[0]):
+#             net[0][i].collector = input_data[i]
+#             print(net[0][i].collector)
+#
+#     print()
+#     print("Output Layer: ")
+#
+#     #print(len(input_data))
+#     #print(sum(input_data))
+#
+#
+#     for i in np.nditer(input_data):
+#         #print(i)
+#         for n in net[i]:
+#             #print(n)
+#             #print(net[i-1])
+#             n.connections = net[i-1] # assign n.connections to net[i-1] to avoid summnation errors
+#             #print(n.connections)
+#
+#             if n.connections:
+#                 n.weights = [random.random()] * len(n.connections)
+#             for index in range(len(n.connections)):
+#                 #print(c)
+#                 c = n.connections[index]
+#                 w = n.weights[index]
+#                 n.collector = n.collector + (c.collector * w)
+#
+#     print(net[2][0].collector)
+#     # with open('doneANN.pickle', 'wb') as handle:
+#     #     pickle.dump(net, handle)
+#
+#
+# main()
